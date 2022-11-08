fe17597f57cfb2ee6db83142797465e032418b00
ChaoSXDemon <ChaoSXDemon@gmail.com>
2022-11-07 20:35:31 -0800

    Implemented terrain generation with height samples from Simplex2D noise function. Normals are calculated by taking the near-by 6 vertices and scaled by their respective areas.

--- private/Producer/TerrainProducer.h
+++ private/Producer/TerrainProducer.h
@@ -11,7 +11,7 @@ class TerrainProducer : public IProducer
 {
 
 public:
-	explicit TerrainProducer(uint32_t width, uint32_t height);
+	explicit TerrainProducer(uint32_t x_vertices, uint32_t z_vertices, uint32_t width, uint32_t height, uint32_t max_elevation);
 	TerrainProducer(const TerrainProducer& rhs) = default;
 	TerrainProducer& operator=(const TerrainProducer& rhs) = default;
 	TerrainProducer(TerrainProducer&& rhs) = default;
@@ -21,8 +21,11 @@ public:
 	virtual void Execute(SceneDatabase* pSceneDatabase) override;
 
 private:
-	uint32_t m_width;	// vertices in x-axis
-	uint32_t m_height;	// vertices in z-axis
+	uint32_t m_numVerticesInX;	// vertices in x-axis
+	uint32_t m_numVerticesInZ;	// vertices in z-axis
+	uint32_t m_width;			// dist between x-axis vertices
+	uint32_t m_height;			// dist between z-axis vertices
+	uint32_t m_maxElevation;	// highest possible elevation; we start at 0.
 
 };
 
