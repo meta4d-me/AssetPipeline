89940659c5b31d2414874fd636b907e20a5248bc
ChaoSXDemon <ChaoSXDemon@gmail.com>
2022-12-18 11:07:07 -0800

    Update TerrainProducer.h
    temp (squash me)

--- private/Producer/TerrainProducer.h
+++ private/Producer/TerrainProducer.h
@@ -18,7 +18,21 @@ class Mesh;
 namespace cdtools
 {
 
-struct TerrainQuad {
+struct TerrainGenParams
+{
+	uint16_t numSectorsInX;									// number of sectors in x-axis
+	uint16_t numSectorsInZ;									// number of sectors in z-axis
+	uint16_t numQuadsInSectorInX;							// number of quads in x-axis for each sector
+	uint16_t numQuadsInSectorInZ;							// number of quads in z-axis for each sector
+	uint16_t quadLengthInX;									// the length of each quad in x-axis
+	uint16_t quadLengthInZ;									// the length of each quad in z-axis
+	int32_t minHeight;										// the minimum height (can be negative) of this entire terrain
+	int32_t maxHeight;										// the maximum height of this entire terrain
+	std::vector<std::pair<float, int64_t>> weightAndSeeds;	// Set of weights and seeds at powers of 2 frequencies
+};
+
+struct TerrainQuad 
+{
 	uint32_t leftTriPolygonId;
 	uint32_t rightTriPolygonId;
 	uint32_t bottomLeftVertexId;
@@ -48,6 +62,7 @@ private:
 	TerrainQuad CreateQuadAt(uint32_t& currentVertexId, uint32_t& currentPolygonId) const;
 	float GetHeightAt(uint32_t x, uint32_t z, const std::vector<std::pair<float, int64_t>>& freq_params, float power_exp) const;
 
+	uint16_t m_numSectors;		// number of sectors to generate
 	uint32_t m_numQuadsInX;		// quads in x-axis
 	uint32_t m_numQuadsInZ;		// quads in z-axis
 	uint32_t m_quadWidth;		// width of a quad
