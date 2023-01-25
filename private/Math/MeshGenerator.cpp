41324e980be802ed610f4aa963f43a718534700d
Jiajun <75730859+T-rvw@users.noreply.github.com>
2023-01-26 01:11:17 +0800

    remove duplicated vertices box (#106)

--- private/Math/MeshGenerator.cpp
+++ private/Math/MeshGenerator.cpp
@@ -39,15 +39,28 @@ std::optional<Mesh> MeshGenerator::Generate(const Box& box, const VertexFormat& 
 	mesh.SetVertexPosition(6U, Point(min.x(), min.y(), min.z()));
 	mesh.SetVertexPosition(7U, Point(max.x(), min.y(), min.z()));
 
+	// TODO : used by braycentric coordinates.
+	if (vertexFormat.Contains(VertexAttributeType::Bitangent))
+	{
+		mesh.SetVertexBiTangent(0U, cd::Vec3f(1.0f, 0.0f, 0.0f));
+		mesh.SetVertexBiTangent(1U, cd::Vec3f(0.0f, 1.0f, 0.0f));
+		mesh.SetVertexBiTangent(2U, cd::Vec3f(0.0f, 0.0f, 1.0f));
+		mesh.SetVertexBiTangent(3U, cd::Vec3f(1.0f, 0.0f, 0.0f));
+		mesh.SetVertexBiTangent(4U, cd::Vec3f(0.0f, 0.0f, 1.0f));
+		mesh.SetVertexBiTangent(5U, cd::Vec3f(1.0f, 0.0f, 0.0f));
+		mesh.SetVertexBiTangent(6U, cd::Vec3f(0.0f, 1.0f, 0.0f));
+		mesh.SetVertexBiTangent(7U, cd::Vec3f(0.0f, 0.0f, 1.0f));
+	}
+
 	// Index buffer
 	mesh.SetPolygon(0U, VertexID(0U), VertexID(1U), VertexID(2U));
 	mesh.SetPolygon(1U, VertexID(1U), VertexID(3U), VertexID(2U));
 	mesh.SetPolygon(2U, VertexID(4U), VertexID(6U), VertexID(5U));
 	mesh.SetPolygon(3U, VertexID(5U), VertexID(6U), VertexID(7U));
-	mesh.SetPolygon(4U, VertexID(0U), VertexID(2U), VertexID(4U));
-	mesh.SetPolygon(5U, VertexID(4U), VertexID(2U), VertexID(6U));
-	mesh.SetPolygon(6U, VertexID(1U), VertexID(5U), VertexID(3U));
-	mesh.SetPolygon(7U, VertexID(5U), VertexID(7U), VertexID(3U));
+	mesh.SetPolygon(4U, VertexID(0U), VertexID(2U), VertexID(6U));
+	mesh.SetPolygon(5U, VertexID(4U), VertexID(0U), VertexID(6U));
+	mesh.SetPolygon(6U, VertexID(1U), VertexID(3U), VertexID(7U));
+	mesh.SetPolygon(7U, VertexID(5U), VertexID(1U), VertexID(7U));
 	mesh.SetPolygon(8U, VertexID(0U), VertexID(4U), VertexID(1U));
 	mesh.SetPolygon(9U, VertexID(4U), VertexID(5U), VertexID(1U));
 	mesh.SetPolygon(10U, VertexID(2U), VertexID(3U), VertexID(6U));
@@ -83,101 +96,6 @@ std::optional<Mesh> MeshGenerator::Generate(const Box& box, const VertexFormat& 
 	//		vertexNormal.Normalize();
 	//	}
 	//}
-
-	return mesh;
-}
-
-std::optional<Mesh> MeshGenerator::GenerateNonUnique(const Box& box, const VertexFormat& vertexFormat)
-{
-	assert(vertexFormat.Contains(VertexAttributeType::Position));
-
-	const Vec3f& min = box.Min();
-	const Vec3f& max = box.Max();
-	Vec3f extent = max - min;
-
-	//          0 --- 1 (max)
-	//        / |   / |
-	//       4 --- 5  |
-	//       |  |  |  |
-	//       |  2 -|- 3
-	//       | /   | /
-	// (min) 6 --- 7
-
-	Mesh mesh(36U, 12U);
-
-	// Vertex positions
-	// 0 - 1 - 3 - 2
-	mesh.SetVertexPosition(0U, Point(min.x(), max.y(), max.z()));
-	mesh.SetVertexPosition(1U, Point(max.x(), max.y(), max.z()));
-	mesh.SetVertexPosition(2U, Point(min.x(), min.y(), max.z()));
-	mesh.SetVertexPosition(3U, Point(max.x(), max.y(), max.z()));
-	mesh.SetVertexPosition(4U, Point(max.x(), min.y(), max.z()));
-	mesh.SetVertexPosition(5U, Point(min.x(), min.y(), max.z()));
-
-	// 5 - 1 - 3 - 7
-	mesh.SetVertexPosition(6U, Point(max.x(), max.y(), max.z()));
-	mesh.SetVertexPosition(7U, Point(max.x(), min.y(), max.z()));
-	mesh.SetVertexPosition(8U, Point(max.x(), max.y(), min.z()));
-	mesh.SetVertexPosition(9U, Point(max.x(), min.y(), max.z()));
-	mesh.SetVertexPosition(10U, Point(max.x(), min.y(), min.z()));
-	mesh.SetVertexPosition(11U, Point(max.x(), max.y(), min.z()));
-
-	// 4 - 5 - 7 - 6
-	mesh.SetVertexPosition(12U, Point(min.x(), max.y(), min.z()));
-	mesh.SetVertexPosition(13U, Point(max.x(), max.y(), min.z()));
-	mesh.SetVertexPosition(14U, Point(min.x(), min.y(), min.z()));
-	mesh.SetVertexPosition(15U, Point(max.x(), max.y(), min.z()));
-	mesh.SetVertexPosition(16U, Point(max.x(), min.y(), min.z()));
-	mesh.SetVertexPosition(17U, Point(min.x(), min.y(), min.z()));
-
-	// 0 - 4 - 6 - 2
-	mesh.SetVertexPosition(18U, Point(min.x(), max.y(), max.z()));
-	mesh.SetVertexPosition(19U, Point(min.x(), max.y(), min.z()));
-	mesh.SetVertexPosition(20U, Point(min.x(), min.y(), max.z()));
-	mesh.SetVertexPosition(21U, Point(min.x(), max.y(), min.z()));
-	mesh.SetVertexPosition(22U, Point(min.x(), min.y(), min.z()));
-	mesh.SetVertexPosition(23U, Point(min.x(), min.y(), max.z()));
-
-	// 0 - 1 - 5 - 4
-	mesh.SetVertexPosition(24U, Point(min.x(), max.y(), max.z()));
-	mesh.SetVertexPosition(25U, Point(max.x(), max.y(), max.z()));
-	mesh.SetVertexPosition(26U, Point(max.x(), max.y(), min.z()));
-	mesh.SetVertexPosition(27U, Point(min.x(), max.y(), max.z()));
-	mesh.SetVertexPosition(28U, Point(max.x(), max.y(), min.z()));
-	mesh.SetVertexPosition(29U, Point(min.x(), max.y(), min.z()));
-
-	// 2 - 3 - 7 - 6
-	mesh.SetVertexPosition(30U, Point(min.x(), min.y(), max.z()));
-	mesh.SetVertexPosition(31U, Point(max.x(), min.y(), max.z()));
-	mesh.SetVertexPosition(32U, Point(max.x(), min.y(), min.z()));
-	mesh.SetVertexPosition(33U, Point(min.x(), min.y(), max.z()));
-	mesh.SetVertexPosition(34U, Point(max.x(), min.y(), min.z()));
-	mesh.SetVertexPosition(35U, Point(min.x(), min.y(), min.z()));
-	
-	// TODO : used by braycentric coordinates.
-	if (vertexFormat.Contains(VertexAttributeType::Bitangent))
-	{
-		uint32_t vertexIndex = 0U;
-		for (uint32_t polygonIndex = 0U; polygonIndex < 12U; ++polygonIndex)
-		{
-			uint32_t aIndex = vertexIndex++;
-			uint32_t bIndex = vertexIndex++;
-			uint32_t cIndex = vertexIndex++;
-			mesh.SetVertexBiTangent(aIndex, cd::Vec3f(1.0f, 0.0f, 0.0f));
-			mesh.SetVertexBiTangent(bIndex, cd::Vec3f(0.0f, 1.0f, 0.0f));
-			mesh.SetVertexBiTangent(cIndex, cd::Vec3f(0.0f, 0.0f, 1.0f));
-		}
-	}
-
-	// Index buffer
-	uint32_t vertexIndex = 0U;
-	for (uint32_t polygonIndex = 0U; polygonIndex < 12U; ++polygonIndex)
-	{
-		VertexID v0(vertexIndex++);
-		VertexID v1(vertexIndex++);
-		VertexID v2(vertexIndex++);
-		mesh.SetPolygon(polygonIndex, v0, v1, v2);
-	}
 
 	return mesh;
 }
