ad1c3563640d9f1f60a2c03308b06e7c379e1881
t-rvw <429601557@qq.com>
2023-03-04 17:41:17 +0800

    test cdbin to cdbin

---
+++ examples/CatDogToCatDog/Main.cpp
@@ -0,0 +1,32 @@
+#include "CDConsumer.h"
+#include "Framework/Processor.h"
+#include "CDProducer.h"
+#include "Utilities/PerformanceProfiler.h"
+
+int main(int argc, char** argv)
+{
+	// argv[0] : exe name
+	// argv[1] : input file path
+	// argv[2] : output file path
+	if (argc != 3)
+	{
+		return 1;
+	}
+
+	using namespace cdtools;
+
+	PerformanceProfiler profiler("AssetPipeline");
+
+	const char* pInputFilePath = argv[1];
+	const char* pOutputFilePath = argv[2];
+	CDProducer producer(pInputFilePath);
+	CDConsumer consumer(pOutputFilePath);
+	consumer.SetExportMode(ExportMode::PureBinary);
+
+	Processor processor(&producer, &consumer);
+	processor.SetDumpSceneDatabaseEnable(true);
+	processor.SetValidateSceneDatabaseEnable(true);
+	processor.Run();
+
+	return 0;
+}
