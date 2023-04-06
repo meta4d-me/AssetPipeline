7092048739f2cf32bb202086d508e0a06b4033a3
Jiajun <75730859+T-rvw@users.noreply.github.com>
2023-04-07 01:35:13 +0800

    adjust light api (#149)

--- private/Scene/LightImpl.h
+++ private/Scene/LightImpl.h
@@ -18,52 +18,57 @@ public:
 	LightImpl() = delete;
 	explicit LightImpl(InputArchive& inputArchive);
 	explicit LightImpl(InputArchiveSwapBytes & inputArchive);
-	explicit LightImpl(LightID lightID, const LightType type);
+	explicit LightImpl(LightID lightID, LightType type);
 	LightImpl(const LightImpl&) = default;
 	LightImpl& operator=(const LightImpl&) = default;
 	LightImpl(LightImpl&&) = default;
 	LightImpl& operator=(LightImpl&&) = default;
 	~LightImpl() = default;
 
-	void Init(LightID lightID, const LightType type);
-	const LightID& GetID() const { return m_id; }
-	const LightType& GetType() const { return m_type; }
+	void Init(LightID lightID, LightType type);
 
-	void SetIntensity(const float intensity) { m_intensity = intensity; }
+	LightID GetID() const { return m_id; }
+	LightType GetType() const { return m_type; }
+
+	void SetName(std::string name) { m_name = cd::MoveTemp(name); }
+	std::string& GetName() { return m_name; }
+	const std::string& GetName() const { return m_name; }
+
+	void SetIntensity(float intensity) { m_intensity = intensity; }
 	float& GetIntensity() { return m_intensity; }
-	const float& GetIntensity() const { return m_intensity; }
+	float GetIntensity() const { return m_intensity; }
 
-	void SetRange(const float range) { m_range = range; }
+	void SetRange(float range) { m_range = range; }
 	float& GetRange() { return m_range; }
-	const float& GetRange() const { return m_range; }
+	float GetRange() const { return m_range; }
 
-	void SetRadius(const float radius) { m_radius = radius; }
+	void SetRadius(float radius) { m_radius = radius; }
 	float& GetRadius() { return m_radius; }
-	const float& GetRadius() const { return m_radius; }
+	float GetRadius() const { return m_radius; }
 
-	void SetWidth(const float width) { m_width = width; }
+	void SetWidth(float width) { m_width = width; }
 	float& GetWidth() { return m_width; }
-	const float& GetWidth() const { return m_width; }
+	float GetWidth() const { return m_width; }
 
-	void SetHeight(const float height) { m_height = height; }
+	void SetHeight(float height) { m_height = height; }
 	float& GetHeight() { return m_height; }
-	const float& GetHeight() const { return m_height; }
+	float GetHeight() const { return m_height; }
 
-	void SetAngleScale(const float angleScale) { m_angleScale = angleScale; }
+	void SetAngleScale(float angleScale) { m_angleScale = angleScale; }
 	float& GetAngleScale() { return m_angleScale; }
-	const float& GetAngleScale() const { return m_angleScale; }
+	float GetAngleScale() const { return m_angleScale; }
 
-	void SetAngleOffeset(const float angleOffeset) { m_angleOffeset = angleOffeset; }
-	float& GetAngleOffeset() { return m_angleOffeset; }
-	const float& GetAngleOffeset() const { return m_angleOffeset; }
-
-	void SetPosition(cd::Point position) { m_position = cd::MoveTemp(position); }
-	cd::Point& GetPosition() { return m_position; }
-	const cd::Point& GetPosition() const { return m_position; }
+	void SetAngleOffset(float angleOffset) { m_angleOffset = angleOffset; }
+	float& GetAngleOffset() { return m_angleOffset; }
+	float GetAngleOffset() const { return m_angleOffset; }
 
 	void SetColor(cd::Vec3f color) { m_color = cd::MoveTemp(color); }
 	cd::Vec3f& GetColor() { return m_color; }
 	const cd::Vec3f& GetColor() const { return m_color; }
+
+	void SetPosition(cd::Point position) { m_position = cd::MoveTemp(position); }
+	cd::Point& GetPosition() { return m_position; }
+	const cd::Point& GetPosition() const { return m_position; }
 
 	void SetDirection(cd::Direction direction) { m_direction = cd::MoveTemp(direction); }
 	cd::Direction& GetDirection() { return m_direction; }
@@ -73,47 +78,28 @@ public:
 	cd::Direction& GetUp() { return m_up; }
 	const cd::Direction& GetUp() const { return m_up; }
 
-	const std::pair<float, float> CalculateScaleAndOffeset(const float innerAngle, const float outerAngle) const;
+	std::pair<float, float> CalculateScaleAndOffset(float innerAngle, float outerAngle) const;
 
 	template<bool SwapBytesOrder>
 	LightImpl& operator<<(TInputArchive<SwapBytesOrder>& inputArchive)
 	{
 		uint32_t lightID;
 		uint8_t lightType;
-
-		float lightIntensity, lightRange, lightRadius,
-			lightWidth, lightHeight, lightAngleScale, lightAngleOffeset;
-
-		inputArchive >> lightID >> lightType >> lightIntensity >> lightRange >>
-			lightRadius >> lightWidth >> lightHeight >> lightAngleScale >> lightAngleOffeset;
-
+		inputArchive >> lightID >> lightType;
 		Init(LightID(lightID), static_cast<LightType>(lightType));
-		SetIntensity(lightIntensity);
-		SetRange(lightRange);
-		SetRadius(lightRadius);
-		SetWidth(lightWidth);
-		SetHeight(lightHeight);
-		SetAngleScale(lightAngleScale);
-		SetAngleOffeset(lightAngleOffeset);
-
-		inputArchive.ImportBuffer(&GetPosition());
-		inputArchive.ImportBuffer(&GetColor());
-		inputArchive.ImportBuffer(&GetDirection());
-		inputArchive.ImportBuffer(&GetUp());
-
+		inputArchive >> GetIntensity() >> GetRange() >> GetRadius()
+			>> GetWidth() >> GetHeight() >> GetAngleScale() >> GetAngleOffset()
+			>> GetColor() >> GetPosition() >> GetDirection() >> GetUp();
 		return *this;
 	}
 
 	template<bool SwapBytesOrder>
 	const LightImpl& operator>>(TOutputArchive<SwapBytesOrder>& outputArchive) const
 	{
-		outputArchive << GetID().Data() << static_cast<uint8_t>(GetType()) << GetIntensity() << GetRange() <<
-			GetRadius() << GetWidth() << GetHeight() << GetAngleScale() << GetAngleOffeset();
-
-		outputArchive.ExportBuffer(&GetPosition(), 1);
-		outputArchive.ExportBuffer(&GetColor(), 1);
-		outputArchive.ExportBuffer(&GetDirection(), 1);
-		outputArchive.ExportBuffer(&GetUp(), 1);
+		outputArchive << GetID().Data() << static_cast<uint8_t>(GetType())
+			<< GetIntensity() << GetRange() << GetRadius()
+			<< GetWidth() << GetHeight() << GetAngleScale() << GetAngleOffset()
+			<< GetColor() << GetPosition() << GetDirection() << GetUp();
 
 		return *this;
 	}
@@ -121,13 +107,15 @@ public:
 private:
 	LightID m_id;
 	LightType m_type;
+	std::string m_name;
+
 	float m_intensity;
 	float m_range;
 	float m_radius;
 	float m_width;
 	float m_height;
 	float m_angleScale;
-	float m_angleOffeset;
+	float m_angleOffset;
 
 	cd::Point m_position;
 	cd::Vec3f m_color;
