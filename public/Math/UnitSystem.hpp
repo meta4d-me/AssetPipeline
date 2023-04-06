67bf10c48e170b047c03b12996169ee160e118aa
t-rvw <429601557@qq.com>
2023-04-06 15:57:18 +0800

    add unit

---
+++ public/Math/UnitSystem.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+namespace cd
+{
+
+enum class Unit
+{
+	CenterMeter,
+	Meter,
+};
+
+// TODO : UnitSystem class.
+// Currently, it seems that we only need to store unit enum is enough.
+// So need more information to decide.
+
+}
