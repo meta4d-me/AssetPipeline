35a151610b303d717ddc6ba35f2cf8a4b792315d
Jiajun <75730859+T-rvw@users.noreply.github.com>
2023-05-11 18:56:29 +0800

    add convenient enum with names macro (#166)

--- public/Scene/LightType.h
+++ public/Scene/LightType.h
@@ -5,36 +5,6 @@
 namespace cd
 {
 
-enum class LightType : uint8_t
-{
-	Point = 0,
-	Directional,
-	Spot,
+DEFINE_ENUM_WITH_NAMES(LightType, Point, Directional, Spot, Sphere, Disk, Rectangle, Tube);
 
-	// Areal light
-	Sphere,
-	Disk,
-	Rectangle,
-	Tube,
-
-	Count,
-};
-
-constexpr const char *LightTypeName[] =
-{
-	"Point",
-	"Directional",
-	"Spot",
-	"Sphere",
-	"Disk",
-	"Rectangle",
-	"Tube",
-};
-
-CD_FORCEINLINE const char *GetLightTypeName(LightType lightType) {
-	return LightTypeName[static_cast<size_t>(lightType)];
 }
-
-static_assert(static_cast<int>(LightType::Count) == sizeof(LightTypeName) / sizeof(char *), "Light type and names mismatch.");
-
-} // namespace cd
