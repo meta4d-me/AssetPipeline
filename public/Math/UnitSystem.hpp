df74f2ef61ff57f34147b9e07ce5967825b4d023
t.rvw <429601557@qq.com>
2023-07-08 14:01:01 +0800

    add unit

--- public/Math/UnitSystem.hpp
+++ public/Math/UnitSystem.hpp
@@ -5,9 +5,28 @@ namespace cd
 
 enum class Unit
 {
+	None,
 	CenterMeter,
 	Meter,
+	Degree,
+	Angle,
+	Lumen
 };
+
+constexpr const char* UnitNames[] =
+{
+	"",
+	"cm",
+	"m",
+	"deg",
+	"rad",
+	"lm"
+};
+
+constexpr const char* GetUnitName(Unit unit)
+{
+	return UnitNames[static_cast<int>(unit)];
+}
 
 // TODO : UnitSystem class.
 // Currently, it seems that we only need to store unit enum is enough.
