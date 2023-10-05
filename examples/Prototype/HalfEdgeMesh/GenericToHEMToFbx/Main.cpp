f0d7fcac264f7f89f7d3426ce1edef55b0ff78bb
t.rvw <429601557@qq.com>
2023-10-05 22:09:26 +0800

    update

--- examples/Prototype/HalfEdgeMesh/GenericToHEMToFbx/Main.cpp
+++ examples/Prototype/HalfEdgeMesh/GenericToHEMToFbx/Main.cpp
@@ -34,8 +34,6 @@ int main(int argc, char** argv)
 	// Processing
 	for (auto& mesh : pSceneDatabase->GetMeshes())
 	{
-		mesh.RemoveDuplicatedVertices();
-
 		auto halfEdgeMesh = cd::HalfEdgeMesh::FromIndexedMesh(mesh);
 		assert(halfEdgeMesh.IsValid());
 		{
