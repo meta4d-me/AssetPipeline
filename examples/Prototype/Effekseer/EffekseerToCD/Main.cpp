8c19cbc19f33404baff217c67f7b1bb34e684152
OVOAOVO <2410651402@qq.com>
2023-10-13 15:07:21 +0800

    efkefc

--- examples/Prototype/Effekseer/EffekseerToCD/Main.cpp
+++ examples/Prototype/Effekseer/EffekseerToCD/Main.cpp
@@ -18,7 +18,7 @@ int main(int argc, char** argv)
 	PerformanceProfiler profiler("AssetPipeline");
 
 	// TODO : how to input char16_t string from argv?
-	const char16_t* pInputFilePath = u"S:/Effekseer/Examples/Resources/Laser01.efkefc";
+	const char16_t* pInputFilePath = u"D:/Effekseer/EffekSeer/Examples/Resources/Laser01.efkefc";
 	const char* pOutputFilePath = argv[2];
 	EffekseerProducer producer(pInputFilePath);
 	CDConsumer consumer(pOutputFilePath);
