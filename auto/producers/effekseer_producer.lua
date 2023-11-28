93acf60356e84b3a0241a10ec23bedc2f41815eb
T-rvw <429601557@qq.com>
2023-11-28 12:51:33 +0800

    add findfbx module and refact makefiles

--- auto/producers/effekseer_producer.lua
+++ auto/producers/effekseer_producer.lua
@@ -1,34 +1,13 @@
 --------------------------------------------------------------
-print("Building effekseer_producer")
+-- EffekseerProducer
+--------------------------------------------------------------
+print("[EffekseerProducer] Generate project...")
 
 -- Define producer dll
 project("EffekseerProducer")
 	kind("SharedLib")
-	language("C++")
 	Platform_SetCppDialect()
-	dependson { "AssetPipelineCore" }
-
-	location(path.join(RootPath, "build"))
-
-	filter { "configurations:Debug" }
-		objdir(path.join(RootPath, "build/obj/Debug"))
-		targetdir(path.join(RootPath, "build/bin/Debug"))
-		libdirs(path.join(RootPath, "build/bin/Release"))
-		links {
-			path.join(RootPath, "build/bin/Debug/AssetPipelineCore")
-		}		
-	filter { "configurations:Release" }
-		objdir(path.join(RootPath, "build/obj/Release"))
-		targetdir(path.join(RootPath, "build/bin/Release"))
-		libdirs(path.join(RootPath, "build/bin/Release"))
-		links {
-			path.join(RootPath, "build/bin/Release/AssetPipelineCore")
-		}
-	filter {}
-
-	defines {
-		"TOOL_BUILD_SHARED"
-	}
+	Tool_InitProject()
 
 	files {
 		path.join(RootPath, "public/Producers/EffekseerProducer/**.*"),
@@ -42,9 +21,9 @@ project("EffekseerProducer")
 		},
 	}
 	
-	commercialSDKIncludeDirs = {}
-	commercialSDKLibDirs = {}
-	commercialSDKLibNames = {}
+	local commercialSDKIncludeDirs = {}
+	local commercialSDKLibDirs = {}
+	local commercialSDKLibNames = {}
 	
 	local config = CommercialSDKConfigs["EFFEKSEER_SDK_DIR"]
 	if config then
@@ -56,8 +35,6 @@ project("EffekseerProducer")
 	end
 
 	includedirs {
-		path.join(RootPath, "public"),
-		path.join(RootPath, "private"),
 		table.unpack(commercialSDKIncludeDirs)
 	}
 
